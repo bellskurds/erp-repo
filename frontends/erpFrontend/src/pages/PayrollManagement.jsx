4bc5cec6eed3f87c24167d88fb700f2765dacb09
topdev1104 <strongman20021104@gmail.com>
2023-07-03 17:16:37 +0300

    updated

--- frontends/erpFrontend/src/pages/PayrollManagement.jsx
+++ frontends/erpFrontend/src/pages/PayrollManagement.jsx
@@ -120,7 +120,7 @@ const PayrollManagement = () => {
           period_label: `${currentDate.format('MMMM')}-1(${currentDate.year()})`,
           year: currentDate.year(),
           periods: getPeriods(currentDate.month(), currentDate.year(), 0),
-          payroll_amount: getPaymentData(JSON.parse(JSON.stringify(assignedEmployee)), allHours, currentDate.year(), currentDate.month(), getPeriods(currentDate.month() + 1, currentDate.year(), 0))
+          payroll_amount: getPaymentData(JSON.parse(JSON.stringify(assignedEmployee)), allHours, currentDate.year(), currentDate.month(), getPeriods(currentDate.month() + 1, currentDate.year(), 0), 1)
           ,
           services_amount: getPaymentData(JSON.parse(JSON.stringify(assignedEmployee)), allHours, currentDate.year(), currentDate.month(), getPeriods(currentDate.month() + 1, currentDate.year(), 0), 2)
 
@@ -132,9 +132,9 @@ const PayrollManagement = () => {
             period_label: `${currentDate.format('MMMM')}-2(${currentDate.year()})`,
             year: currentDate.year(),
             periods: getPeriods(currentDate.month(), currentDate.year(), 1),
-            payroll_amount: getPaymentData(JSON.parse(JSON.stringify(assignedEmployee)), allHours, currentDate.year(), currentDate.month(), getPeriods(currentDate.month() + 1, currentDate.year(), 1))
+            payroll_amount: getPaymentData(JSON.parse(JSON.stringify(assignedEmployee)), allHours, currentDate.year(), currentDate.month(), getPeriods(currentDate.month() + 1, currentDate.year(), 1), 1)
             ,
-            services_amount: getPaymentData(JSON.parse(JSON.stringify(assignedEmployee)), allHours, currentDate.year(), currentDate.month(), getPeriods(currentDate.month() + 1, currentDate.year(), 0), 2)
+            services_amount: getPaymentData(JSON.parse(JSON.stringify(assignedEmployee)), allHours, currentDate.year(), currentDate.month(), getPeriods(currentDate.month() + 1, currentDate.year(), 1), 2)
 
           })
       }
@@ -174,7 +174,7 @@ const PayrollManagement = () => {
     const endDay = parseInt(periods.split("-")[1]);
     const start_date = new Date(year, startDay === 31 ? (month - 2) : (month - 1), startDay);
     const end_date = new Date(year, month - 1, endDay);
-
+    console.log(year, month, periods, start_date, end_date, 11);
     const _listItems = _assignedEmployees.filter(({ contract }) =>
       contract.status === "active" &&
       (
@@ -193,91 +193,92 @@ const PayrollManagement = () => {
     _listItems.map((obj, index) => {
 
       const { contract: assignedContract } = obj;
-
-      obj.sunday_hr = obj.sunday ? getHours(obj.sunday) : 0;
-      obj.monday_hr = obj.monday ? getHours(obj.monday) : 0;
-      obj.tuesday_hr = obj.tuesday ? getHours(obj.tuesday) : 0;
-      obj.wednesday_hr = obj.wednesday ? getHours(obj.wednesday) : 0;
-      obj.thursday_hr = obj.thursday ? getHours(obj.thursday) : 0;
-      obj.friday_hr = obj.friday ? getHours(obj.friday) : 0;
-      obj.saturday_hr = obj.saturday ? getHours(obj.saturday) : 0;
-
-
-      let currentDate = moment(start_date);
-
-      const end = moment(end_date);
-
-      while (currentDate.isSameOrBefore(end)) {
-        const day = currentDate.date();
-        const _day = currentDate.day();
-        const year = currentDate.year();
-        const month = currentDate.month();
-        const dataIndex = `-day-${year}_${month + 1}_${day}`;
-        const dataIndex1 = `_day-${year}_${month + 1}_${day}`;
-        console.log(dataIndex, index);
-        switch (_day) {
-          case 0:
-            obj[dataIndex] = changedCellValue(Hours, `${year}/${month + 1}/${day}`, obj) || obj.sunday_hr;
-            obj[dataIndex1] = (changedCellValue(Hours, `${year}/${month + 1}/${day}`, obj) || obj.sunday_hr) - obj.sunday_hr
-            break;
-
-          case 1:
-            obj[dataIndex] = changedCellValue(Hours, `${year}/${month + 1}/${day}`, obj) || obj.monday_hr;
-            obj[dataIndex1] = (changedCellValue(Hours, `${year}/${month + 1}/${day}`, obj) || obj.monday_hr) - obj.monday_hr
-
-            break;
-
-          case 2:
-            obj[dataIndex] = changedCellValue(Hours, `${year}/${month + 1}/${day}`, obj) || obj.tuesday_hr;
-
-            obj[dataIndex1] = (changedCellValue(Hours, `${year}/${month + 1}/${day}`, obj) || obj.tuesday_hr) - obj.tuesday_hr
-
-            break;
-
-          case 3:
-            obj[dataIndex] = changedCellValue(Hours, `${year}/${month + 1}/${day}`, obj) || obj.wednesday_hr;
-            obj[dataIndex1] = (changedCellValue(Hours, `${year}/${month + 1}/${day}`, obj) || obj.wednesday_hr) - obj.wednesday_hr
-
-            break;
-
-          case 4:
-            obj[dataIndex] = changedCellValue(Hours, `${year}/${month + 1}/${day}`, obj) || obj.thursday_hr;
-            obj[dataIndex1] = (changedCellValue(Hours, `${year}/${month + 1}/${day}`, obj) || obj.thursday_hr) - obj.thursday_hr
-
-            break;
-          case 5:
-            obj[dataIndex] = changedCellValue(Hours, `${year}/${month + 1}/${day}`, obj) || obj.friday_hr;
-            obj[dataIndex1] = (changedCellValue(Hours, `${year}/${month + 1}/${day}`, obj) || obj.friday_hr) - obj.friday_hr
+      if (assignedContract.type === payType) {
+        obj.sunday_hr = obj.sunday ? getHours(obj.sunday) : 0;
+        obj.monday_hr = obj.monday ? getHours(obj.monday) : 0;
+        obj.tuesday_hr = obj.tuesday ? getHours(obj.tuesday) : 0;
+        obj.wednesday_hr = obj.wednesday ? getHours(obj.wednesday) : 0;
+        obj.thursday_hr = obj.thursday ? getHours(obj.thursday) : 0;
+        obj.friday_hr = obj.friday ? getHours(obj.friday) : 0;
+        obj.saturday_hr = obj.saturday ? getHours(obj.saturday) : 0;
 
 
-            break;
-          case 6:
-            obj[dataIndex] = changedCellValue(Hours, `${year}/${month + 1}/${day}`, obj) || obj.saturday_hr;
-            obj[dataIndex1] = (changedCellValue(Hours, `${year}/${month + 1}/${day}`, obj) || obj.saturday_hr) - obj.saturday_hr
+        let currentDate = moment(start_date);
 
-            break;
+        const end = moment(end_date);
 
-          default:
-            break;
-        }
-        currentDate = currentDate.add(1, 'days');
-      };
-      obj.hrs_bi = assignedContract.type === 1 ? mathCeil(obj.hr_week * 4.333 / 2) : getServiceHours(obj);
-      obj.week_pay = mathCeil(obj.hrs_bi * obj.sal_hr)
-      obj.adjustment = calcAdjustment(obj);
-      obj.adjust = calcAdjustment(obj) * obj.sal_hr;
-      obj.salary = parseFloat(obj.adjust) + parseFloat(obj.week_pay);
-      obj.transferencia = assignedContract.type === 1 ? obj.salary : obj.salary * 0.89;
+        while (currentDate.isSameOrBefore(end)) {
+          const day = currentDate.date();
+          const _day = currentDate.day();
+          const year = currentDate.year();
+          const month = currentDate.month();
+          const dataIndex = `-day-${year}_${month + 1}_${day}`;
+          const dataIndex1 = `_day-${year}_${month + 1}_${day}`;
+          console.log(dataIndex, index);
+          switch (_day) {
+            case 0:
+              obj[dataIndex] = changedCellValue(Hours, `${year}/${month + 1}/${day}`, obj) || obj.sunday_hr;
+              obj[dataIndex1] = (changedCellValue(Hours, `${year}/${month + 1}/${day}`, obj) || obj.sunday_hr) - obj.sunday_hr
+              break;
+
+            case 1:
+              obj[dataIndex] = changedCellValue(Hours, `${year}/${month + 1}/${day}`, obj) || obj.monday_hr;
+              obj[dataIndex1] = (changedCellValue(Hours, `${year}/${month + 1}/${day}`, obj) || obj.monday_hr) - obj.monday_hr
+
+              break;
+
+            case 2:
+              obj[dataIndex] = changedCellValue(Hours, `${year}/${month + 1}/${day}`, obj) || obj.tuesday_hr;
+
+              obj[dataIndex1] = (changedCellValue(Hours, `${year}/${month + 1}/${day}`, obj) || obj.tuesday_hr) - obj.tuesday_hr
+
+              break;
+
+            case 3:
+              obj[dataIndex] = changedCellValue(Hours, `${year}/${month + 1}/${day}`, obj) || obj.wednesday_hr;
+              obj[dataIndex1] = (changedCellValue(Hours, `${year}/${month + 1}/${day}`, obj) || obj.wednesday_hr) - obj.wednesday_hr
+
+              break;
+
+            case 4:
+              obj[dataIndex] = changedCellValue(Hours, `${year}/${month + 1}/${day}`, obj) || obj.thursday_hr;
+              obj[dataIndex1] = (changedCellValue(Hours, `${year}/${month + 1}/${day}`, obj) || obj.thursday_hr) - obj.thursday_hr
+
+              break;
+            case 5:
+              obj[dataIndex] = changedCellValue(Hours, `${year}/${month + 1}/${day}`, obj) || obj.friday_hr;
+              obj[dataIndex1] = (changedCellValue(Hours, `${year}/${month + 1}/${day}`, obj) || obj.friday_hr) - obj.friday_hr
+
+
+              break;
+            case 6:
+              obj[dataIndex] = changedCellValue(Hours, `${year}/${month + 1}/${day}`, obj) || obj.saturday_hr;
+              obj[dataIndex1] = (changedCellValue(Hours, `${year}/${month + 1}/${day}`, obj) || obj.saturday_hr) - obj.saturday_hr
+
+              break;
+
+            default:
+              break;
+          }
+          currentDate = currentDate.add(1, 'days');
+        };
+        obj.hrs_bi = assignedContract.type === 1 ? mathCeil(obj.hr_week * 4.333 / 2) : getServiceHours(obj);
+        obj.week_pay = mathCeil(obj.hrs_bi * obj.sal_hr)
+        obj.adjustment = calcAdjustment(obj);
+        obj.adjust = calcAdjustment(obj) * obj.sal_hr;
+        obj.salary = parseFloat(obj.adjust) + parseFloat(obj.week_pay);
+        obj.transferencia = assignedContract.type === 1 ? obj.salary : obj.salary * 0.89;
+      }
     });
     let calValue = 0;
 
     _listItems.map(obj => {
       const { contract } = obj;
       if (contract.type === payType) {
-        console.log(obj.transferencia, 'obj.transferencia');
         calValue += parseFloat(obj.transferencia)
       }
     })
+    // console.log(calValue, 'calValue');
     return calValue;
   }
   const calcAdjustment = (record) => {
