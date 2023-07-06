18d7e7219b1e033ef319dc54d131ee2cf9b8fa26
topdev1104 <strongman20021104@gmail.com>
2023-07-06 16:45:23 +0300

    updated

--- frontends/erpFrontend/src/app/Navigation/index.jsx
+++ frontends/erpFrontend/src/app/Navigation/index.jsx
@@ -98,10 +98,10 @@ export default function Navigation() {
             Payment Invoice
           </Menu.Item> */}
 
-          <Menu.Item key={'Admin'} icon={<TeamOutlined />}>
+          {/* <Menu.Item key={'Admin'} icon={<TeamOutlined />}>
             <Link to={'/admin'} />
             Admin
-          </Menu.Item>
+          </Menu.Item> */}
           <SubMenu key={'Settings'} icon={<SettingOutlined />} title={'Settings'}>
             <Menu.Item key={'PaymentMode'}>
               <Link to={'/payment/mode'} />
