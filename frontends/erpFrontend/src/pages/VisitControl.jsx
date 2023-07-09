b74c7fea6264696ebbaa2900ac09116afd6d1f43
topdev1104 <strongman20021104@gmail.com>
2023-07-09 08:45:53 +0300

    updated

--- frontends/erpFrontend/src/pages/VisitControl.jsx
+++ frontends/erpFrontend/src/pages/VisitControl.jsx
@@ -1,119 +1,24 @@
-import { DashboardLayout, DefaultLayout } from '@/layout';
-import { DashOutlined, DeleteOutlined, EditOutlined, EyeOutlined, LeftOutlined, RightOutlined, SafetyOutlined, SearchOutlined, SkinOutlined, TeamOutlined } from '@ant-design/icons';
-import { Button, Col, DatePicker, Form, Input, InputNumber, Layout, Modal, Popconfirm, Radio, Row, Select, Space, Table, Tag, Typography, message } from 'antd';
-import Search from 'antd/lib/transfer/search';
+import { DashboardLayout, } from '@/layout';
+import { LeftOutlined, RightOutlined, SafetyOutlined, SearchOutlined, SkinOutlined, TeamOutlined } from '@ant-design/icons';
+import { Button, Col, DatePicker, Form, Input, Layout, Modal, Radio, Row, Select, Table, message } from 'antd';
 import React, { useCallback, useEffect, useRef, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 import { crud } from '@/redux/crud/actions';
 import { selectListItems } from '@/redux/crud/selectors';
-import { Link } from 'react-router-dom/cjs/react-router-dom';
 import { request } from '@/request';
 import moment from 'moment';
 import SelectAsync from '@/components/SelectAsync';
-import { useContext } from 'react';
-const EditableContext = React.createContext(null);
 
-const statusLabel = ["", "Active", "Canceled", "Finished"]
-const statusArr = [
-  { value: 0, label: "all" },
-  { value: 1, label: "Active" },
-  { value: 2, label: "Canceled" },
-  { value: 3, label: "Finished" },
-];
-const statusArr1 = [
-  { value: 1, label: "Active" },
-  { value: 2, label: "Canceled" },
-  { value: 3, label: "Finished" },
-];
-const EditableRow = ({ index, ...props }) => {
-  const [form] = Form.useForm();
-  return (
-    <Form form={form} component={false}>
-      <EditableContext.Provider value={form}>
-        <tr {...props} />
-      </EditableContext.Provider>
-    </Form>
-  );
-};
-const EditableCell = ({
-  title,
-  editable,
-  children,
-  dataIndex,
-  record,
-  handleSave,
-  ...restProps
-}) => {
-  const [editing, setEditing] = useState(false);
-  const inputRef = useRef(null);
-  const form = useContext(EditableContext);
-  useEffect(() => {
-    if (editing) {
-      inputRef.current.focus();
-    }
-  }, [editing]);
-  const toggleEdit = () => {
-    setEditing(!editing);
-    form.setFieldsValue({
-      [dataIndex]: record[dataIndex],
-    });
-  };
-  const save = async () => {
-    try {
-      const values = await form.validateFields();
-
-      toggleEdit();
-      handleSave({
-        ...record,
-        ...values,
-      }, values);
-    } catch (errInfo) {
-      console.log('Save failed:', errInfo);
-    }
-  };
-  let childNode = children;
-  if (editable) {
-    childNode = editing ? (
-      <Form.Item
-        style={{
-          margin: 0,
-        }}
-        name={dataIndex}
-        rules={[
-          {
-            required: true,
-            message: `${title} is required.`,
-          },
-        ]}
-      >
-        <Input ref={inputRef} onPressEnter={save} onBlur={save} />
-      </Form.Item>
-    ) : (
-      <div
-        className="editable-cell-value-wrap"
-        style={{
-          paddingRight: 24,
-        }}
-        onClick={toggleEdit}
-      >
-        {children}
-      </div>
-    );
-  }
-  return <td {...restProps}>{childNode}</td>;
-};
 const VisitControl = () => {
   const entity = "visitControl"
   const [isModalVisible, setIsModalVisible] = useState(false);
   const [searchText, setSearchText] = useState('');
   const [filterData, setFilterData] = useState([]);
-
   const [isUpdate, setIsUpdate] = useState(false);
-  const [rangeDate, setRangeDate] = useState();
+  const [rangeDate] = useState();
   const [currentMonth, setCurrentMonth] = useState(new Date().getMonth() + 1);
   const [currentYear, setCurrentYear] = useState(new Date().getFullYear());
-
   const visitType = [
     '',
     <TeamOutlined />,
@@ -130,13 +35,9 @@ const VisitControl = () => {
   const handleOk = () => {
     setIsModalVisible(false);
   };
-
   const handleCancel = () => {
     setIsModalVisible(false);
   };
-  const generateCustomerId = () => {
-    return new Date().valueOf();
-  }
   const [customerStores, setCustomerStores] = useState([]);
   const [globalData, setGlobalData] = useState([]);
   const [filteredStores, setFilteredStores] = useState([]);
@@ -147,12 +48,8 @@ const VisitControl = () => {
   const [editingKey, setEditingKey] = useState('');
   const [currentId, setCurrentId] = useState('');
   const [currentItem, setCurrentItem] = useState({});
-  const [references, setReferences] = useState([]);
-  const [initEmployeeColumns, setInitEmployeeColumns] = useState([]);
   const [paginations, setPaginations] = useState([])
-  const [endDate, setEndDate] = useState();
-  const [status, setStatus] = useState();
-  const [summatoryCost, setSummatoryCost] = useState();
+
   const isEditing = (record) => record._id === editingKey;
   const editItem = (item) => {
 
@@ -173,21 +70,7 @@ const VisitControl = () => {
       setIsUpdate(true);
     }
   }
-  const deleteItem = (item) => {
-    const id = item._id;
-    dispatch(crud.delete({ entity, id }))
-    setTimeout(() => {
-      dispatch(crud.resetState());
-      dispatch(crud.list({ entity }));
-    }, 1000)
-  }
-  const getDateLabel = (date) => {
-    if (!date) return '';
-    const start = moment(date[0]);
-    const end = moment(date[1]);
-    return `${start.format("MMMM")}${start.date()}(${start.year()}) - ${end.format("MMMM")}${end.date()}(${end.year()})`
-  }
-  const typeArr = ["", "Residential", "Commercial"]
+
   const columns = [
     {
       title: 'Client(Branch)',
@@ -254,12 +137,7 @@ const VisitControl = () => {
     // },
 
   ];
-  const components = {
-    body: {
-      row: EditableRow,
-      cell: EditableCell,
-    },
-  };
+
   const mergedColumns = columns.map((col) => {
     if (!col.editable) {
       return col;
@@ -303,208 +181,7 @@ const VisitControl = () => {
   const onFinishFailed = (errorInfo) => {
     console.log('Failed:', errorInfo);
   };
-  const getObjectDiff = (obj1, obj2) => {
-    const diff = [];
 
-    // Check for changed properties and values
-    for (let i = 0; i < obj1.length; i++) {
-      const obj1Props = Object.keys(obj1[i]);
-      const obj2Props = Object.keys(obj2[i]);
-      const changedProps = [];
-
-      for (let prop of obj1Props) {
-        if (obj2[i].hasOwnProperty(prop) && obj1[i][prop] !== obj2[i][prop]) {
-          changedProps.push({ [prop]: obj2[i][prop] });
-        }
-      }
-
-      if (changedProps.length > 0) {
-        diff.push({ index: i, changes: changedProps });
-      }
-    }
-
-    // Check for added properties and values
-    for (let i = 0; i < obj2.length; i++) {
-      const obj2Props = Object.keys(obj2[i]);
-
-      for (let prop of obj2Props) {
-        let found = false;
-
-        for (let j = 0; j < obj1.length; j++) {
-          if (obj1[j].hasOwnProperty(prop)) {
-            found = true;
-            break;
-          }
-        }
-
-        if (!found) {
-          diff.push({ index: i, added: { [prop]: obj2[i][prop] } });
-        }
-      }
-    }
-
-    return diff;
-  }
-  const ExtraWeek = () => {
-    const start_ = endDate.date();
-    const end_ = start_ + 7;
-
-    let currentDate = endDate;
-    let dates = []; // Array to store the next 7 days
-    for (let i = start_; i < end_; i++) {
-      dates.push({ title: currentDate.format('YYYY-MM-DD'), dataIndex: `day_${currentDate.format('YYYY-MM-DD')}`, editable: true, render: (text) => { return (text || 0) } }); // Add the formatted date to the array
-      currentDate = currentDate.add(1, 'day'); // Increment the current date by 1 day
-    }
-
-    setInitEmployeeColumns([...initEmployeeColumns, ...dates]);
-  }
-
-  const addEmployee = () => {
-    const defaultObj = {};
-    for (var i = 0; i < initEmployeeColumns.length; i++) {
-      var { dataIndex } = initEmployeeColumns[i];
-      if (dataIndex.includes('day_')) {
-        defaultObj[dataIndex] = 0;
-      }
-    }
-    setEmployeeList([...employeeList, { key: new Date().valueOf(), ...defaultObj }])
-  }
-  useEffect(() => {
-
-    const Columns = [
-      {
-        title: "Employee",
-        dataIndex: "employee",
-        render: (_, record) => {
-          return (
-            <SelectAsync entity={"employee"} displayLabels={["name"]} onChange={(e) => changeEmployee(e, record)} value={_} />
-          );
-        }
-      },
-      {
-        title: "Total",
-        dataIndex: "total",
-        render: (_, record) => {
-          return (totalHours(record) || 0);
-        }
-      },
-    ]
-    let currentDate = moment(new Date());
-    let dates = []; // Array to store the next 7 days
-    if (employeeList && employeeList.length) {
-      let totalCost = 0;
-      employeeList.map(item => {
-        Object.keys(item).map(key => {
-          if (key.includes('day_')) {
-
-            totalCost += parseFloat(item[key]);
-
-            console.log(totalCost);
-          }
-        })
-      });
-      formRef.current.setFieldsValue({ cost: totalCost })
-
-      setSummatoryCost(totalCost);
-
-
-
-      const item = employeeList[0];
-      Object.keys(item).map((key, index) => {
-        if (key.includes('day_')) {
-          dates.push({
-            title: key.split('day_')[1],
-            dataIndex: key,
-            editable: true,
-          })
-          const end = moment(key.split('day_')[1]);
-          end.add(1, 'day')
-          setEndDate(end)
-        }
-      })
-    } else {
-      for (let i = 0; i < 7; i++) {
-        dates.push({
-          title: currentDate.format('YYYY-MM-DD'),
-          dataIndex: `day_${currentDate.format('YYYY-MM-DD')}`,
-          editable: true,
-        }); // Add the formatted date to the array
-        currentDate = currentDate.add(1, 'day'); // Increment the current date by 1 day
-
-        setEndDate(currentDate);
-      }
-    }
-
-    setInitEmployeeColumns([...Columns, ...dates]);
-  }, [employeeList]);
-
-  useEffect(() => {
-    console.log(filteredStores, 'filteredStores');
-  }, [filteredStores])
-
-  // useEffect(() => {
-  //   dispatch(crud.resetState());
-  //   dispatch(crud.list({ entity }));
-
-  //   async function init() {
-  //     const { result } = await request.listById({ entity: 'customerStores' });
-
-  //     setCustomerStores(result);
-  //     // setReferences(result);
-  //   }
-  //   init();
-  // }, []);
-  useEffect(() => {
-    // setFilterData(items);
-    setPaginations(pagination)
-  }, [items, pagination])
-  const handleSave = (row, values) => {
-
-    console.log(values, row, '33333');
-    const newData = [...employeeList];
-    const index = newData.findIndex((item) => row.key === item.key);
-    const item = newData[index];
-    newData.splice(index, 1, {
-      ...item,
-      ...row,
-    });
-    setEmployeeList(prev => {
-      const newValue = [...newData];
-      return newValue
-    });
-  };
-  useEffect(() => {
-    const newData = [...employeeList];
-    newData.map(obj => [
-      initEmployeeColumns.map(columns => {
-        const { dataIndex } = columns;
-        if (dataIndex.includes('day_') && !obj.hasOwnProperty(dataIndex)) {
-          obj[dataIndex] = 0;
-        }
-      })
-    ])
-  }, [initEmployeeColumns])
-
-  const totalHours = (record) => {
-    var total = 0;
-    for (var key in record) {
-      if (key.includes('day_')) {
-        total += parseFloat(record[key]);
-      }
-    }
-    return total;
-  }
-
-  const changeEmployee = (value, record) => {
-
-    record.employee = value;
-    const updatedData = employeeList.map((item) => {
-      if (item.key === record.key) {
-        return record;
-      }
-      return item;
-    });
-  }
   useEffect(() => {
     const filteredData = globalData.filter((record) => {
       const { customer, store } = record;
@@ -515,7 +192,7 @@ const VisitControl = () => {
     })
     setFilterData(filteredData);
     setPaginations({ current: 1, pageSize: 10, total: filteredData.length })
-  }, [searchText, status, rangeDate])
+  }, [searchText, rangeDate])
 
   const handelDataTableLoad = useCallback((pagination) => {
     const { current, total } = pagination;
@@ -543,9 +220,7 @@ const VisitControl = () => {
     }
   }
   useEffect(() => {
-
     async function init() {
-
       const startDay = 1;
       const endDay = new Date(currentYear, currentMonth, 0).getDate();
       const start_date = new Date(currentYear, currentMonth - 1, startDay);
@@ -608,29 +283,9 @@ const VisitControl = () => {
   }, [
     currentMonth, currentYear, changeStatus
   ])
-  const dateValue = (value) => {
-    return `${new Date(value).getFullYear()}-${new Date(value).getMonth()}`
-  }
   const getDate = (value) => {
     return moment(value).format("YYYY-MM-DD")
   }
-  const groupArry = (data) => {
-    const groupedData = data.reduce((result, item) => {
-      const key = `${dateValue(item.visit_date)}-${item.customer._id}-${item.store._id}`;
-      if (!result[key]) {
-        result[key] = { date: item.visit_date, customer: item.customer, store: item.store, values: [] };
-      }
-      result[key].values.push(item);
-      // console.log(key, item, 'dfdf');
-      return result;
-    }, {});
-    const groupedArray = Object.values(groupedData);
-
-    return groupedArray;
-  }
-
-
-
   const Footer = () => {
     const pages = paginations
     const { current, count, total, page } = pages
